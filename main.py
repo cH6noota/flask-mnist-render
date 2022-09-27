177215650f1ba9ddbeb0a48247a6f1a85414d36f
cH6noota <morichika.816@gmail.com>
2022-09-28 03:44:45 +0900

    push

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import numpy as np
+import os
 from flask import Flask, render_template, request
-
+from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
-from PIL import Image
+from tensorflow.keras.preprocessing import image
+
+import numpy as np
+
 
 SIZE = 28
 
@@ -17,11 +20,19 @@ def index():
 
 @app.route('/send', methods=['POST'])
 def predict():
-    image = request.files.get("file")
+    file = request.files.get("file")
     text = 'ファイルが正しく選択されていません'
-    if image:
-        img = Image.open(image).convert('L').resize((SIZE, SIZE))
-        pred = model.predict(np.array(img).reshape(1, SIZE, SIZE))
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join("uploads", filename))
+        filepath = os.path.join("uploads", filename)
+        img = image.load_img(filepath, grayscale=True, target_size=(SIZE, SIZE))
+        img = image.img_to_array(img)
+        data = np.array([img])
+        result = model.predict(data)[0]
+        pred = result.argmax()
+        # img = Image.open(image).convert('L').resize((SIZE, SIZE))
+        # pred = model.predict(np.array(img).reshape(1, SIZE, SIZE))
         text = '{}が予測されました'.format(np.argmax(pred))
     return render_template('index.html', text=text)
 
